5a3bc01acc1fd7108a7edb8611ccd677ee2e71e0
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2024-12-23 10:43:15 +0530

    Create P3.c

---
+++ P3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    int flag;
+    printf("Enter a number : ");
+    scanf("%d",&num);
+    for(int i=0;i<=num;i++)
+    {
+        if(i*i==num)
+        {
+            flag = 1;
+            break;
+        }
+        else
+        {
+            flag = 0;
+        }
+    }
+   
+    if(flag == 1)
+    {
+        printf("It is a Perfect Number");
+    }
+    else
+    {
+        printf("It is not a Perfect Number");
+    }
+}
