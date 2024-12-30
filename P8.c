902d948fa5a2abdd4d3cd42b23300e0a605e2db2
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2024-12-30 17:51:59 +0530

    Create P8.c

---
+++ P8.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter no. of rows : ");
+    scanf("%d",&n);
+    for(int row=1;row<=n;row++)
+    {
+        for(int col=1;col<=n-row;col++)
+        {
+            printf(" ");
+        }
+        for(int col=1;col<=2*row-1;col++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
