727e5c65c397dc4c30e04e1ba9b2e8531625b494
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2024-12-30 14:34:31 +0530

    Create P10.c

---
+++ P10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter no. of rows : ");
+    scanf("%d",&n);
+    for(int row=1;row<=n;row++)
+    {
+        for(int col=1;col<=row;col++)
+        {
+            printf("%d ",col);
+        }
+        printf("\n");
+    }
+    return 0;
+}
