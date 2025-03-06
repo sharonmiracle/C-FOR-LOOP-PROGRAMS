6e52f79b628384894a01f303b485d5cf21234ddf
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2025-03-06 11:37:14 +0530

    Create P29.c

---
+++ P29.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter no. of rows : ");
+    scanf("%d",&n);
+    for(int row=1;row<=n;row++)
+    {
+        for(int col=1;col<=n-row;col++)
+        {
+            printf("  ");
+        }
+        for(int col=1;col<=row;col++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for(int row=0;row<n;row++)
+    {
+        for(int col=0;col<=row;col++)
+        {
+            printf("  ");
+        }
+        for(int col=1;col<n-row;col++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
