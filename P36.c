36c1427b5170ba882f9523fec29fc711108fd0b4
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2025-03-08 14:55:41 +0530

    Create P36.c

---
+++ P36.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter no. of of rows : ");
+    scanf("%d",&n);
+
+    for(int i=1; i<=n; i++)
+    {
+        int num = 1;
+        for(int j=0; j<n-i; j++)
+        {
+            printf(" ");
+        }
+        for (int j=0; j<i; j++)
+        {
+            printf("%d ", num);
+            num = num * (i-j) / (j+1);
+        }
+    printf("\n");
+    }
+    return 0;
+}
