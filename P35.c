d3001f7bed79efd1cde59bd9d0995e90e34749cc
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2025-03-08 12:33:04 +0530

    Create P35.c

---
+++ P35.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter no. of rows : ");
+    scanf("%d",&n);
+    for(int row=0;row<n;row++)
+    {
+        for(int col=1;col<=row;col++)
+        {
+            printf(" ");
+        }
+        for(int col=1;col<=n-row;col++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
