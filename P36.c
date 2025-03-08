02122921595183c8ab9902071fc0c562cea92114
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2025-03-08 15:58:22 +0530

    Update P36.c

--- P36.c
+++ P36.c
@@ -5,17 +5,17 @@ int main()
     printf("Enter no. of of rows : ");
     scanf("%d",&n);
 
-    for(int i=1; i<=n; i++)
+    for(int i=0 ; i<n; i++)
     {
         int num = 1;
-        for(int j=0; j<n-i; j++)
+        for(int j=1; j<n-i; j++)
         {
             printf(" ");
         }
-        for (int j=0; j<i; j++)
+        for (int j=0; j<=i; j++)
         {
             printf("%d ", num);
-            num = num * (i-j) / (j+1);
+            num = num * (i-j) / (j+1);   // num = [num * (i-j)] / (j+1)
         }
     printf("\n");
     }
