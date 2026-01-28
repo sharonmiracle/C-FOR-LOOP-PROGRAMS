899cb9b121013b1eff95aca63aa7d87c93094226
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2026-01-28 21:33:24 +0530

    Fix formatting and indentation in P-04.c

--- P-04.c
+++ P-04.c
@@ -4,13 +4,15 @@ int main()
     int n;
     printf("Enter no. of rows : ");
     scanf("%d",&n);
-    for(int row=1;row<=n;row++)
+    
+    for(int row=1; row<=n; row++)
     {
-        for(int col=1;col<=row;col++)
+        for(int col=1; col<=row; col++)
         {
             printf("* ");
         }
         printf("\n");
     }
+    
     return 0;
 }
