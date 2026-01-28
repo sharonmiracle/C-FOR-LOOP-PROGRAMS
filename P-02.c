17eb5e8d315551595a27de022c53d48b92dbd8c3
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2026-01-28 19:39:35 +0530

    Implement input validation for Fibonacci series
    Added input validation for Fibonacci series terms.

--- P-02.c
+++ P-02.c
@@ -4,15 +4,29 @@ int main()
     int n;
     int t1=0, t2=1;
     int t3;
+    
     printf("Enter no. of terms : ");
     scanf("%d",&n);
-    printf("Fibonacci series of %d is %d %d ",n, t1, t2);
-    for(int i=2;i<n;i++)
+    
+    if(n<=0)
     {
-        t3=t1+t2;
-        printf("%d ",t3);
-        t1=t2;
-        t2=t3;
+        printf("Invalid number of terms");
     }
+    else if(n==1)
+    {
+        printf("Fibonacci series of %d is %d",n, t1);
+    }
+    else
+    {
+        printf("Fibonacci series of %d is %d %d ",n, t1, t2);
+        for(int i=2; i<n; i++)
+        {
+            t3 = t1 + t2;
+            printf("%d ",t3);
+            t1 = t2;
+            t2 = t3;
+        }
+    }
+    
     return 0;
 }
