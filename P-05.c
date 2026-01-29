58fae24369451d25fe038f60cd8af909e4d86c42
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2026-01-29 10:47:33 +0530

    Fix formatting issues in P-05.c

--- P-05.c
+++ P-05.c
@@ -4,17 +4,19 @@ int main()
     int n;
     printf("Enter no. of rows : ");
     scanf("%d",&n);
-    for(int row=1;row<=n;row++)
+    
+    for(int row=1; row<=n; row++)
     {
-        for(int col=1;col<=n-row;col++)
+        for(int col=1; col<=n-row; col++)
         {
             printf("  ");
         }
-        for(int col=1;col<=row;col++)
+        for(int col=1; col<=row; col++)
         {
             printf("* ");
         }
         printf("\n");
     }
+    
     return 0;
 }
